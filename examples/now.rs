2c59d18ad20684f1c5acb651487146513b496856
paulc <212168+paulc@users.noreply.github.com>
2025-11-06 21:38:55 +0000

    Implement framed serial reader

--- examples/now.rs
+++ examples/now.rs
@@ -32,6 +32,7 @@ use core::fmt::Write;
 use static_cell::make_static;
 
 use esp_now_bridge::format_mac::format_mac;
+use tinybuf::Buffer;
 
 extern crate alloc;
 
@@ -83,7 +84,7 @@ async fn main(spawner: Spawner) {
     let now_channel = make_static!(Channel::<NoopRawMutex, heapless::Vec<u8, 255>, 2>::new());
 
     spawner.spawn(writer(tx, now_channel.receiver())).unwrap();
-    spawner.spawn(reader(rx)).unwrap();
+    spawner.spawn(frame_reader(rx)).unwrap();
 
     // Main Loop
     let mut ticker = Ticker::every(Duration::from_secs(10));
@@ -157,6 +158,8 @@ async fn main(spawner: Spawner) {
     }
 }
 
+const FRAME_START: [u8; 2] = [0xaa, 0xcc];
+
 #[embassy_executor::task]
 async fn writer(
     mut tx: UsbSerialJtagTx<'static, Async>,
@@ -166,12 +169,10 @@ async fn writer(
         .await
         .unwrap();
     loop {
-        embedded_io_async::Write::write_all(&mut tx, b"XXXYYYZZZ")
-            .await
-            .unwrap();
         let message = channel_rx.receive().await;
-        let len = (message.len() as u16).to_le_bytes();
-        embedded_io_async::Write::write_all(&mut tx, &[0x00, 0xff, 0x00, 0xff])
+        let len = [message.len() as u8];
+        // Send header
+        embedded_io_async::Write::write_all(&mut tx, &FRAME_START)
             .await
             .unwrap();
         embedded_io_async::Write::write_all(&mut tx, &len)
@@ -184,11 +185,93 @@ async fn writer(
     }
 }
 
-const BUFFER_SIZE: usize = 256;
-const USB_BUFFER_SIZE: usize = 64;
+const USB_BUFFER_LEN: usize = 64;
+const MAX_FRAME_LEN: usize = 256; // Including length byte
+const FRAME_HEADER_LEN: usize = 3;
+const FRAME_BUFFER_LEN: usize = MAX_FRAME_LEN + USB_BUFFER_LEN; // Ensure we have enough space from FRAME + BUFFER
+
+struct FrameHeader([u8; FRAME_HEADER_LEN]);
+
+impl FrameHeader {
+    pub fn new_from_buffer<const N: usize>(buf: &mut Buffer<N>) -> Option<Self> {
+        if buf.len() >= FRAME_HEADER_LEN {
+            let mut header = [0_u8; FRAME_HEADER_LEN];
+            buf.copy_to(&mut header[..]);
+            Some(Self(header))
+        } else {
+            None
+        }
+    }
+    pub fn get_length(&self) -> usize {
+        self.0[FRAME_HEADER_LEN - 1] as usize
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum FrameState {
+    Wait,
+    Frame(usize),
+}
 
 #[embassy_executor::task]
-async fn reader(mut rx: UsbSerialJtagRx<'static, Async>) {
+async fn frame_reader(mut rx: UsbSerialJtagRx<'static, Async>) {
+    let mut usb_buf = [0u8; USB_BUFFER_LEN];
+    let mut state = FrameState::Wait;
+    let mut buf = Buffer::<FRAME_BUFFER_LEN>::new();
+    loop {
+        let r = embedded_io_async::Read::read(&mut rx, &mut usb_buf).await;
+        match r {
+            Ok(len) => {
+                defmt::info!(">> Serial RX: {:?}", usb_buf[..len]);
+                // Push data to buffer and look for FRAME_START
+                buf.push(&usb_buf[..len]).expect("Buffer Error"); // safe
+                defmt::info!("Serial RX: {}", len);
+                match state {
+                    FrameState::Wait => {
+                        match buf.find(&FRAME_START) {
+                            Some(i) => {
+                                // Advance to FRAME_START
+                                buf.advance(i);
+                                while let Some(hdr) = FrameHeader::new_from_buffer(&mut buf) {
+                                    let n = hdr.get_length();
+                                    buf.advance(FRAME_HEADER_LEN);
+                                    if buf.len() >= n {
+                                        // We have full frame
+                                        let mut frame = [0_u8; MAX_FRAME_LEN];
+                                        buf.copy_to(&mut frame);
+                                        buf.advance(n);
+                                        state = FrameState::Wait;
+                                        defmt::info!(">> RX FRAME:: {:?}", frame[..n]);
+                                    } else {
+                                        state = FrameState::Frame(n);
+                                        defmt::info!(">> RX HEADER: {}", hdr.get_length());
+                                    }
+                                }
+                            }
+                            None => {}
+                        }
+                    }
+                    FrameState::Frame(n) => {
+                        if buf.len() >= n {
+                            let mut frame = [0_u8; MAX_FRAME_LEN];
+                            buf.copy_to(&mut frame);
+                            buf.advance(n);
+                            state = FrameState::Wait;
+                            defmt::info!(">> RX FRAME:: {:?}", frame[..n]);
+                        }
+                    }
+                }
+            }
+            Err(e) => {
+                defmt::error!("USB read error: {:?}", e);
+            }
+        }
+    }
+}
+
+/*
+#[embassy_executor::task]
+async fn tty_reader(mut rx: UsbSerialJtagRx<'static, Async>) {
     let mut rbuf = [0u8; USB_BUFFER_SIZE];
     let mut string_buffer: heapless::Vec<u8, BUFFER_SIZE> = heapless::Vec::new();
     loop {
@@ -226,3 +309,4 @@ async fn reader(mut rx: UsbSerialJtagRx<'static, Async>) {
         }
     }
 }
+*/
